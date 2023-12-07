aa3d8311295903980b22e6a48c6e4a58c4ed817f
Ernesto Garcia <ergave17@gmail.com>
2023-12-06 22:23:52 -0500

    New version

--- android/app/src/main/java/com/norbitapp/MainActivity.java
+++ android/app/src/main/java/com/norbitapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.norbitapp;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -15,6 +17,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "norbitapp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**

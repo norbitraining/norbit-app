1ea710fdeb0cd98103cbd24123219e0bac473353
Ernesto Garcia <ergave17@gmail.com>
2024-08-14 12:28:22 -0500

    Fix extract version

---
+++ scripts/extractVersion.ts
@@ -0,0 +1,39 @@
+import {execSync} from 'child_process';
+
+function main(packageName: string, serviceAccountKey: string) {
+  try {
+    const command = `google-play tracks get --package-name "${packageName}" --track "production" --credentials "${serviceAccountKey}"`;
+    const result = execSync(command).toString();
+
+    const data = JSON.parse(result);
+    let latestVersionName = '';
+
+    for (const release of data.releases) {
+      if (release.status === 'draft') {
+        latestVersionName = release.name;
+        break;
+      }
+    }
+
+    if (latestVersionName) {
+      console.log(latestVersionName);
+    } else {
+      console.error('No draft version found.');
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error('Error processing the command output:', error);
+    process.exit(1);
+  }
+}
+
+const [, , packageName, serviceAccountKey] = process.argv;
+
+if (!packageName || !serviceAccountKey) {
+  console.error(
+    'Usage: ts-node extractVersion.ts <PACKAGE_NAME> <SERVICE_ACCOUNT_JSON_KEY>',
+  );
+  process.exit(1);
+}
+
+main(packageName, serviceAccountKey);
